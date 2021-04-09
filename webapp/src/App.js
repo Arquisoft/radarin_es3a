05399cf03546b39f60b8be1da09095092f8ee94b
UO264545 <UO264545@uniovi.es>
2021-04-09 12:24:55 +0200

    Notificaciones en App

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -9,10 +9,12 @@ import SignUp from './components/SignUp';
 import UsersLocation from './components/UsersLocation'
 import FriendList from './components/FriendList'
 import MapComponent from './components/map/MapComponent';
-import Footer from './components/footer/Footer' 
+import Footer from './components/footer/Footer'
 import { getToken, onMessageListener } from './services/firebase';
 import { saveUserToken } from './services/updateUserData';
-
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
+import { store } from 'react-notifications-component';
 
 export default function App() {
 
@@ -24,10 +26,27 @@ export default function App() {
 
   onMessageListener().then(payload => {
     console.log(payload);
+    store.addNotification({
+      title: payload.notification.title,
+      message: payload.notification.body,
+      type: "custom",
+      container: "top-center",
+      animationIn: ["animate__animated", "animate__fadeIn"],
+      animationOut: ["animate__animated", "animate__fadeOut"],
+      dismiss: {
+        duration: 3000
+      }
+    });
   }).catch(err => console.log('failed: ', err));
 
   return (
     <Router>
+      <ReactNotification types={[
+          {
+            htmlClasses: ['notification__item--custom'],
+            name: 'custom'
+          }
+        ]}/>
       <Navbar />
       <Switch>
         <Route path='/' exact component={Welcome} />
