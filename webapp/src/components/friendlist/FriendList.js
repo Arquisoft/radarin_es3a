7cf67715e0022c18aeebfc1d45bc3f886bc9e89c
UO258273 <UO258273@uniovi.es>
2021-04-10 20:24:44 +0200

    Creada la interfaz de la lista de amigos

--- webapp/src/components/friendlist/FriendList.js
+++ webapp/src/components/friendlist/FriendList.js
@@ -2,52 +2,107 @@ import React from 'react';
 import { fetchFriends } from '../../services/fetchFriends';
 import { fetchName, fetchPhoto } from '../../services/fetchProfile';
 import './FriendList.css'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
-class FriendList extends React.Component{
+
+class FriendList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {users: []}
-      }
+        this.state = { users: [] }
+    }
 
-    componentDidMount(){
+    componentDidMount() {
         this.fetchUsers()
     }
 
     async fetchUsers() {
         let that = this;
         let users = await fetchFriends();
-        users = users.map(function(u, i) {
+        users = users.map(function (u, i) {
             let user = {
                 webId: u,
                 photo: './user.png',
                 name: ""
             };
-            fetchPhoto(user.webId).then(photo => { 
-                if(!photo)
+            fetchPhoto(user.webId).then(photo => {
+                if (!photo)
                     return
                 users[i].photo = photo;
-                that.setState({users: users})
+                that.setState({ users: users })
             });
-            fetchName(user.webId).then(name =>  {
-                if(!name)
+            fetchName(user.webId).then(name => {
+                if (!name)
                     name = users[i].webId;
                 users[i].name = name
-                that.setState({users: users})
+                that.setState({ users: users })
             });
 
             return user;
         });
-        this.setState({users: users})
+        this.setState({ users: users })
     }
-    
+
     render() {
         return (
             <>
                 <h1>Lista de amigos</h1>
-                <div>
-                    {this.state.users.map(function(user, i){
-                        return <div id={i} key={i}>  <h3>{user.name} </h3> <img className="friend-img" src={ user.photo }/></div>
+                <div >
+                    {this.state.users.map(function (user, i) {
+                        return (
+                            <List>
+                                <ListItem alignItems="flex-start" id={i} key={i}>
+                                    <ListItemAvatar >
+                                        <Avatar style={{
+                                            width: 75,
+                                            height: 75
+                                        }} className="friend-img" alt="Profile Photo" src={user.photo} />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    style={{
+                                                        fontWeight: 600,
+                                                        fontSize: 18
+                                                    }}>
+                                                    {user.name}
+                                                </Typography>
+                                            </React.Fragment>
+                                        }
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    style={{
+                                                        fontWeight: 200,
+                                                        fontSize: 14,
+                                                        color: "whitesmoke"
+                                                    }}>
+                                                    {user.webId}
+                                                </Typography>
+                                            </React.Fragment>
+                                        }
+
+                                    />
+                                </ListItem>
+                                <Divider variant="inset" component="li" />
+                            </List>
+
+                        );
                     })}
+                </div>
+                <div>
+                    <Box style={{
+                        width: '100%',
+                        height: '6em',
+                    }}>
+                    </Box>
                 </div>
             </>
         )
