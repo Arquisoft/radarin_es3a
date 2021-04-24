3f36ddcad44d707b682bc76a8ee89adda384ea25
UO258273 <UO258273@uniovi.es>
2021-04-24 23:41:34 +0200

    Funcionalidad de  borrado de usuarios

--- webapp/src/components/admin/Admin.js
+++ webapp/src/components/admin/Admin.js
@@ -3,23 +3,10 @@ import React from 'react';
 import { fetchName } from '../../services/fetchProfile';
 import './Admin.css'
 import { getUsers } from '../../api/api';
-<<<<<<< Updated upstream
-// import {deleteFromDB} from '../../services/deleteFromDB';
+import {deleteUser} from '../../services/deleteFromDB';
 import ListGroup from "react-bootstrap/ListGroup";
 import { Button, Container, Row, Col } from 'react-bootstrap';
 
-=======
-import {deleteUser} from '../../services/deleteFromDB';
-import solidAuth from 'solid-auth-client';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
->>>>>>> Stashed changes
 
 
 class Admin extends React.Component {
@@ -38,10 +25,12 @@ class Admin extends React.Component {
         try {
             let users = await getUsers();
             let usersFiltrados = [];
+            let todosLosUsers
             for (let index in users) {
                 try {
+                    //let u = await fetchName(users[index].webId);
+                    //todosLosUsers.push(u)
                     let user = await fetchName(users[index].webId);
-
                     if (user != "radarin") {
                         usersFiltrados.push(user)
                     }
@@ -51,63 +40,15 @@ class Admin extends React.Component {
             }
             console.log(usersFiltrados)
             this.setState({ users: usersFiltrados });
+            //this.setState({ users: todosLosUsers });
         }
         catch (error) {
             console.log("Error fetching user list from restapi. Is it on?")
         }
     }
 
+
     render() {
-<<<<<<< HEAD
-        return (
-            <>
-                <div class="row align-items-start">
-<<<<<<< Updated upstream
-                    <h1 class="titulo">Opciones de Administrador</h1>
-                </div>
-                <div class="container">
-=======
-                    <div class="col">
-
-                        <div class="container" >
-                            <div class="row align-items-start">
-                                <h1 class="titulo">Opciones de Administrador</h1>
-                            </div>
-                            <div class="col" >
-                                <h2 class="">Manejo Usuarios</h2>
-                                <ListGroup>
-                                    {this.state.users.map(function (user, i) {
-                                        return <ListGroup.Item id={i} key={i}>{user.webId }
-                                         <Button onClick={() => deleteUser(user.webId)} >Enviar notificación</Button>
-                                         </ListGroup.Item>
-                                    })}
-                                </ListGroup>
-                            </div>
-                            <div class="col" >
-                                <h2 class="">Usuarios Conectados</h2>
-                            </div>
->>>>>>> Stashed changes
-
-                    <div class="row">
-                        <div class="col-sm">
-                            <h2 class="">Manejo Usuarios</h2>
-                            <ListGroup>
-                                {this.state.users.map(function (user, i) {
-                                    return <ListGroup.Item id={i} key={i}class="list-group"><li class="list-group-item">{user}</li>
-                                        {/* <Button type="button" class="btn btn-danger" onClick={() => deleteFromDB(user.webId)} >Enviar notificación</Button> */}
-                                    </ListGroup.Item>
-                                })}
-                            </ListGroup>
-                        </div>
-                        <div class="col-sm">
-                            <h2 class="">Usuarios Conectados</h2>
-                            <ListGroup>
-                                {this.state.users.map(function (user, i) {
-                                    return <ListGroup.Item id={i} key={i}>{user.webId}
-                                    </ListGroup.Item>
-                                })}
-                            </ListGroup>
-=======
         if (!this.state.users) {
             return (<div class="d-flex justify-content-center">
                 <div class="spinner-border" role="status">
@@ -130,7 +71,7 @@ class Admin extends React.Component {
                                     <ListGroup>
                                         {this.state.users.map(function (user, i) {
                                             return <ListGroup.Item id={i} key={i} class="list-group"><li class="list-group-item">{user}</li>
-                                                {/* <Button type="button" class="btn btn-danger" onClick={() => deleteFromDB(user.webId)} >Enviar notificación</Button> */}
+                                                {<Button type="button" class="btn btn-danger" onClick={() => deleteUser(user.webId)} >Eliminar usuario</Button>}
                                             </ListGroup.Item>
                                         })}
                                     </ListGroup>
@@ -146,21 +87,10 @@ class Admin extends React.Component {
                                         })}
                                     </ListGroup>
                                 </div>
-<<<<<<< HEAD
-                            </div>
->>>>>>> Admin-Danger-Zone
-                        </div>
-                    </div>
-
-
-
-
-=======
                             </Col>
                         </Row>
                     </Container>
                 </Container>
->>>>>>> Admin-Danger-Zone
                 </>
             )
         }
