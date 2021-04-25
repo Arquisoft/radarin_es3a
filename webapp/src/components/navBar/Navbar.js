1cb0e8693e59b05eaee13cf229731981e7e39170
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-04-25 13:32:36 +0200

    navbar fin

--- webapp/src/components/navBar/Navbar.js
+++ webapp/src/components/navBar/Navbar.js
@@ -37,14 +37,13 @@ function Navbar() {
   return (
     <>
       <nav class="navbar  navbar-expand-lg navbar-dark bg-dark ">
-        {/* <a class="navbar-brand" href="#">Navbar</a> */}
-        <img src='/navbarIcon.svg' to='/' className='logo' class="navbar-brand d-print-inline w-25 p-3" alt="Radarin" ></img>
-        <Link to='/' className='nav-links' onClick={closeMobileMenu} class=" navbar-logo" >RADARIN</Link>
+        
+        <Link to='/' className='nav-links' onClick={closeMobileMenu} class=" navbar-logo" ><p class="logoLetra"><img src='/navbarIcon.svg' to='/' className='logo' class="navbar-brand " alt="Radarin" ></img>RADARIN</p></Link>
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"  aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown" class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}>
+        <div class="collapse navbar-collapse  text-light bg-dark" id="navbarNavDropdown" class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}>
           <ul class="navbar-nav">
             <li class="nav-item active">
               <Link to='/' className='nav-links' onClick={closeMobileMenu}  class="nav-link">Home</Link>
@@ -77,86 +76,12 @@ function Navbar() {
             </LoggedIn>
             <LoggedOut>
               <li class='nav-item'>
-                <Link to='/sign-in' type="button" className='nav-links' onClick={closeMobileMenu}>Sign In</Link>
+                <Link to='/sign-in' type="button" className='nav-links' onClick={closeMobileMenu}  class='btn btn-primary'>Sign In</Link>
               </li>
             </LoggedOut>
           </ul>
         </div>
       </nav>
-      {/* <nav  className='navbar'>
-        
-        <img src='/navbarIcon.svg' to='/' className='logo' alt="Radarin" />
-        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-          RADARIN
-        </Link>
-        
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-              Home
-            </Link>
-          </li>
-          <LoggedIn>
-          <li className='nav-item'>
-            <Link
-              to='/map'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Map
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/admin'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Admin
-            </Link>
-          </li>
-          <li
-            className='nav-item'
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-          >
-            <Link
-              to='/friend-list'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Friends <i className='fas fa-caret-down' />
-            </Link>
-            {dropdown && <Dropdown />}
-          </li>
-          </LoggedIn>
-          <li className='nav-item'>
-            <Link
-              to='/about-us'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              About us
-            </Link>
-          </li>
-        </ul>
-        <LoggedIn>
-        <li className='nav-item'>
-          <LogoutButton className='btnL' onClick={closeMobileMenu}>Log out</LogoutButton>
-          </li>
-          </LoggedIn>
-          <LoggedOut>
-        <li className='nav-item'>
-        <Link
-              to='/sign-in'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Sign In
-            </Link>
-          </li>
-          </LoggedOut>
-      </nav> */}
     </>
   );
 }
