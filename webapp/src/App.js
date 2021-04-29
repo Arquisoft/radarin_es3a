bc1884c6247e2d42a3330c331d94d9bb58f408a5
uo271620 <adolfo.r.s@hotmail.es>
2021-04-30 01:07:31 +0200

    Añadido al master seguridad en rutas

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import Navbar from './components/navBar/Navbar';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {useLoggedIn} from '@solid/react';
 import Welcome  from './components/welcome/Welcome';
 import SignIn from './components/signIn/SignIn';
 import AboutUs from './components/aboutUs/AboutUs';
@@ -17,8 +18,10 @@ import Footer from './components/footer/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import MapContainer from './components/map/MapContainer';
 
-export default function App() {
 
+var log;
+export default function App() {
+  log = FuntionGetLoggedUser();
   useEffect(() => {
     // Obtener token de app móvil
     if(window.Android) 
@@ -28,7 +31,7 @@ export default function App() {
         saveUserToken(token);
       });
   }, []);
-
+  
   onMessageListener().then(payload => {
     showNotification(payload.notification)
   }).catch(err => console.log('failed: ', err));
@@ -44,12 +47,12 @@ export default function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Welcome} />
-        <Route path='/map' component={MapContainer} />
+        <PrivateRoute path='/map' component={MapContainer} />
         <Route path='/sign-in'  component={SignIn} />
         <Route path='/about-us'  component={AboutUs} />
-        <Route path='/locations' component={UsersLocation} />
-        <Route path='/admin' component={Admin} />
-        <Route path='/friend-list' component={FriendList} />
+        <PrivateRoute path='/locations' component={UsersLocation} />
+        <PrivateRoute path='/admin' component={Admin} />
+        <PrivateRoute path='/friend-list' component={FriendList} />
       </Switch>
       <Footer/>
     </Router>
@@ -91,3 +94,16 @@ function prepareToAndroidNotifications() {
     showNotification(notification)
   })
 }
+
+function FuntionGetLoggedUser() {
+  const loggedIn = useLoggedIn();
+  return loggedIn;
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    log
+    ? <Component {...props} />
+    : <Redirect to='/sign-in'/> 
+    )}/>
+)
