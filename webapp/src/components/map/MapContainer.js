5c9da7bf7814c8fd6eeb33b363a56af6a9a99f3f
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-04-29 17:01:46 +0200

    footer map arreglado

--- webapp/src/components/map/MapContainer.js
+++ webapp/src/components/map/MapContainer.js
@@ -18,7 +18,7 @@ export default function MapContainer() {
     }
 
     return (
-        <div className="container-fluid  p-0 m-0">
+        <div className="contenedorMapContainer container-fluid  p-0 m-0">
             <div className="row">
                 <div className="col md-12">
 
