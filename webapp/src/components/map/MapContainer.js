acab1517cf6a1415924415eb43431ddbe07b5abf
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-04-29 14:30:42 +0200

    arreglado boostrap mapa

--- webapp/src/components/map/MapContainer.js
+++ webapp/src/components/map/MapContainer.js
@@ -3,7 +3,9 @@ import { useState } from "react";
 import MapOff from './MapOff';
 import { saveUserLocation } from '../../services/updateUserData';
 import { Button } from '@material-ui/core';
-import SliderRadio from './SliderRadio';
+import SliderContainer from './SliderContainer';
+import MutedComponent from './MutedComponent';
+
 
 export default function MapContainer() {
 
@@ -16,22 +18,34 @@ export default function MapContainer() {
     }
 
     return (
-        <div className="container-fluid">
-            <div className="col md-12">
-                <div className="row">
-                    <Button className="" onClick={() => switchOnOff()}>{on ? 'Apagar' : 'Encender'}</Button>
-                    <div>
-                        <div className="container">
-                            <span className="text-light p-1 w-25">Distancia deseada: </span>
-                            <SliderRadio />
-                            {/* <input type="text" value={radioBusqueda} onChange={handleChange}  ></input> */}
-                            {/* <span className="text-light p-1 w-25">Value: {radioBusqueda} </span> */}
+        <div className="container-fluid  p-0 m-0">
+            <div className="row">
+                <div className="col md-12">
+
+                    <div className="container-fluid p-0 m-0">
+                        <div className="row">
+                            <div className="col md-6 ">
+                                <Button className="" onClick={() => switchOnOff()}>{on ? 'Apagar' : 'Encender'}</Button>
+                            </div>
+                            <div className="col md-6">
+                                <div className="container-fluid p-0 m-0">
+                                    {on
+                                        ? <SliderContainer />
+                                        : <MutedComponent />}
+                                </div>
+                            </div>
+
+
                         </div>
-                    </div>
-                    <div className="row">
-                        {on
-                            ? <MapComponent></MapComponent>
-                            : <MapOff></MapOff>}
+                        <div className="row">
+                            <div className="col md-6">
+                                <div className="container-fluid p-0 m-0">
+                                    {on
+                                        ? <MapComponent></MapComponent>
+                                        : <MapOff></MapOff>}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
