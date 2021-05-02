283f911cbff384cfeb7266438a69da5359f2d65e
Error404UsernameNotFound <raulmr44@gmail.com>
2021-05-02 17:52:31 +0200

    Borrados archivos innecesarios.

--- webapp/src/services/addToTypeIndex.js
+++
@@ -1,9 +0,0 @@
-import { rdf, solid } from "rdf-namespaces";
-
-export async function addToTypeIndex (typeIndex, document, forClass) {
-  const typeRegistration = typeIndex.addSubject();
-  typeRegistration.addRef(rdf.type, solid.TypeRegistration);
-  typeRegistration.addRef(solid.instance, document.asRef());
-  typeRegistration.addRef(solid.forClass, forClass);
-  return typeIndex.save([ typeRegistration ]);
-}
